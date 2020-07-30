918364ed0c2dd1f395e7020bbc088acc0e4aa45b
codwerk <louis_widdy@live.fr>
2020-07-30 17:09:05 +0200

    final commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 const onglets = document.querySelectorAll('.onglets');
-const contenu = document.querySelectorAll('.tabs__container-contenu');
+const contenus = document.querySelectorAll('.tabs__container-contenu');
 let index = 0;
 
-console.log(onglets, contenu);
+console.log(onglets, contenus);
 
 onglets.forEach( onglet => {
   onglet.addEventListener('click', () => {
@@ -21,6 +21,14 @@ onglets.forEach( onglet => {
 
       if(onglets[i].getAttribute('data-anim') != index) {
         onglets[i].classList.remove('active');
+      }
+    }
+
+    for (let j = 0; j < contenus.length; j++) {
+      if(contenus[j].getAttribute('data-anim') == index) {
+        contenus[j].classList.add('active-contenu');
+      } else {
+        contenus[j].classList.remove('active-contenu');
       }
     }
 
